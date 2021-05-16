d4bf49c5fd37bb888b7201c86a2ed917d061893d
Antonis Karaminas <a.karaminas@pan-net.eu>
2021-05-16 10:28:04 +0300

    echo to files

--- scripts/ecr.sh
+++ scripts/ecr.sh
@@ -17,10 +17,11 @@ for i in "${repo_names[@]}"
 do
 echo "Scanning repo: $i"
 result=`aws ecr describe-images --profile homeos-aws-prd  --repository-name $i --query 'sort_by(imageDetails,& imagePushedAt)[-1]'  | grep findingSeverityCounts | awk '{print $2}'`
-if [[ $result == "{}" ]]
+if [[ $result == "{" ]]
 then
     repo_with_findings+=($i)
-else
+elif  [[ $result != "{}" ]]
+then
     repo_failed_scans+=($i)
 fi
 done
@@ -28,6 +29,6 @@ done
 echo "all_repos=${#repo_names[@]}"
 echo "repo_with_findings=${#repo_with_findings[@]}"
 echo "failed scanned repos=${#repo_failed_scans[@]}"
-
-
-
+echo ${repo_names[@]} | sed 's/ /\n/g' > repo_names.txt
+echo ${repo_with_findings[@]}| sed 's/ /\n/g' > repo_with_findings.txt
+echo ${repo_failed_scans[@]} | sed 's/ /\n/g' > repo_failed_scans.txt
