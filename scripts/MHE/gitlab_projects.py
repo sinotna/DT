d834ee33f91658327efdc109393db999df4b0e55
Antonis Karaminas <a.karaminas@eu.telekom.com>
2024-02-05 11:53:49 +0200

    get token from os env.I have exported the env

--- scripts/MHE/gitlab_projects.py
+++ scripts/MHE/gitlab_projects.py
@@ -1,1 +1,12 @@
 #!/usr/local/Caskroom/miniconda/base/bin/python
+
+import os
+import gitlab
+
+# Gitlab server ulr
+gitlab_url = 'https://gitlab.devops.telekom.de'
+
+# Get the gitlab access token from os env variables
+access_token = os.environ.get('GITLAB_ACCESS_TOKEN')
+
+print(access_token)
