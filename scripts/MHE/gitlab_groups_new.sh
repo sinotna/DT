dcadfa23b94ffb7cbcc4123c0644012f46b090d0
Antonis Karaminas <a.karaminas@eu.telekom.com>
2024-02-07 12:49:06 +0200

    subgroup_id fixed. Global variable changed from i to x

--- scripts/MHE/gitlab_groups_new.sh
+++ scripts/MHE/gitlab_groups_new.sh
@@ -11,8 +11,8 @@ group_id=41110
 # List project inside a subgroup, search for any gitlab-ci file and search the tag fields.
 
 list_projects () {
-    local subgroup_id="$1"
-    local project_url=$(curl --silent --header "Private-Token:$GITLAB_ACCESS_TOKEN" "https://gitlab.devops.telekom.de/api/v4/groups/$subgroup_id/projects" | jq -r '.[].http_url_to_repo')
+    local project_id="$1"
+    local project_url=$(curl --silent --header "Private-Token:$GITLAB_ACCESS_TOKEN" "https://gitlab.devops.telekom.de/api/v4/groups/$project_id/projects" | jq -r '.[].http_url_to_repo')
     #Convert subgroups_id string to an array.
     project_url_array=()
     while IFS= read -r id; do
@@ -20,7 +20,7 @@ list_projects () {
     done <<< "$project_url"
     for i in ${project_url_array[@]}
         do
-        git clone $i
+            git clone $i
         done
 }
 
@@ -29,7 +29,6 @@ list_projects () {
 # List all groups and subgroups unde mhe
 
 list_subgroups ()  {
-
     local subgroup_id="$1"
     echo "subgroup_id = $subgroup_id"
     local subgroups_id=$(curl --silent --header "Private-Token:$GITLAB_ACCESS_TOKEN" "https://gitlab.devops.telekom.de/api/v4/groups/$subgroup_id/subgroups" | jq -r '.[].id')
@@ -39,10 +38,13 @@ list_subgroups ()  {
         subgroups_id_array+=("$id")
     done <<< "$subgroups_id"
     ###
-    for i in ${subgroups_id_array[@]}
+    
+    for x in ${subgroups_id_array[@]}
         do
-        list_subgroups $i        
+        list_projects "$x"
+        list_subgroups "$x"        
         done
+
 }
 
 list_subgroups "$group_id"
